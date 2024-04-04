5ebfc846823b4ceea0c8340dd7bccac7a6e4fc01
tundeao <tunde.arabaowoyele@gmail.com>
2024-04-04 10:23:15 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import express from "express";
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5.js";
+import cors from "cors"
+import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
+
+
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.use(express.json());
+Hello(app);
+Lab5(app);
+CourseRoutes(app);
+ModuleRoutes(app);
+app.listen(process.env.PORT || 4000);
